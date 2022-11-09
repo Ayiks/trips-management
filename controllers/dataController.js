22fce2bfbe945ec940b3e573dd518f805f9d4ad6
Ayiks <cliffysarp@gmail.com>
2022-11-09 22:58:31 +0000

    updats

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -63,6 +63,294 @@
 //         } )
 //     }
 //   })
-let dataTable = new DataTable("#myTable");
+//let dataTable = new DataTable("#myTable");
+
+const user1 = JSON.parse(localStorage.getItem('user'))
+
+function selectV(vehicleName){
+    const select = document.getElementById('selectV')
+    // create option using DOM
+   
+const newOption = document.createElement('option');
+select.appendChild(newOption)
+const optionText = document.createTextNode(vehicleName);
+
+newOption.appendChild(optionText);
+
+newOption.setAttribute('option','Option Value');
+    // const options = document.createElement('option')
+    // select.appendChild(options)
+    // select.value = vehicleName
+    // options.innerHTML = vehicleName
+}
+fetch('https://kayhans-backend-app.herokuapp.com/vehicleRecords/records',)
+    .then((response)=>response.json())
+    .then((data)=>{
+        data.forEach(element => {
+            selectV(element.vehicleName)
+        });
+    })
 
 
+
+function selectR(rate){
+        const select = document.getElementById('rate')
+        // create option using DOM
+       
+    const newOption = document.createElement('option');
+    select.appendChild(newOption)
+    const optionText = document.createTextNode(rate);
+    
+    newOption.appendChild(optionText);
+    
+    newOption.setAttribute('option','Option Value');
+        // const options = document.createElement('option')
+        // select.appendChild(options)
+        // select.value = vehicleName
+        // options.innerHTML = vehicleName
+    }
+    fetch('https://kayhans-backend-app.herokuapp.com/vehicleRecords/rates',)
+    .then((response)=>response.json())
+    .then((data)=>{
+        data.forEach(element => {
+            selectV(element.rate)
+        });
+    })
+
+  function addVehicle() {
+    let vehicleName = document.getElementById('vehicleName')
+    let vehicleNumber = document.getElementById('vehicleNumber')
+    let vehicleType = document.getElementById('vehicleType')
+    let vehicleColor = document.getElementById('vehicleColor')
+    
+    let data = {
+        vehicleName : vehicleName.value,
+        category: vehicleType.value,
+        vehicleNumber: vehicleNumber.value,
+        color: vehicleColor.value
+    }
+
+    if (vehicleName.value !="") {
+        
+        fetch(`https://kayhans-backend-app.herokuapp.com/vehicleRecords/records`, {
+        method: 'POST',
+        mode: 'cors',
+        headers:{
+            'Accept':'*',
+            'Content-Type': 'application/json',
+            'token': `Bearer ${user1.accessToken}`,
+        },
+        body: JSON.stringify(data)
+        })
+        .then((res)=>res.json())
+        .then((data)=>{
+        console.log(data);
+        window.alert('data saved successfully')
+        window.location.href="../dashboard.html"
+    
+    })
+    .catch((error)=>{
+        window.alert(error)
+    })
+    } else {
+        alert('Please fill out all fileds!')
+    }
+    
+   
+
+    }
+
+//=========EMPLOYEES==========//
+function users(id, username, email, startDate, userId){
+    const tableBody = document.getElementById('tbody')
+    const tableRow = document.createElement('tr')
+
+    tableBody.appendChild(tableRow)
+
+    const tdId = document.createElement('td')
+    const tdUsername = document.createElement('td')
+    const tdEmail = document.createElement('td')
+    const tdStartDate = document.createElement('td')
+    const td4 = document.createElement('td')
+
+    const tdDiv = document.createElement('div')
+    const editIcon = document.createElement('i')
+    const delteIcon = document.createElement('i')
+    const deleteIconLink = document.createElement('a')
+    const editIconLink = document.createElement('a')
+    editIcon.classList.add("bi","bi-pencil", "p-2")
+    delteIcon.classList.add("bi","bi-trash")
+
+    deleteIconLink.appendChild(delteIcon)
+    editIconLink.appendChild(editIcon)
+
+    tdDiv.append(editIconLink,deleteIconLink)    
+    td4.appendChild(tdDiv)
+
+    tdId.innerHTML =id
+    tdUsername.innerHTML = username
+    tdEmail.innerHTML = email
+    tdStartDate.innerHTML = startDate
+
+    tableRow.append(tdId, tdUsername, tdEmail,tdStartDate, td4)
+
+    editIconLink.href = `../users-profile.html?id=${userId}`
+
+
+
+}
+fetch('https://kayhans-backend-app.herokuapp.com/vehicleRecords/users')
+.then((response) => response.json())
+.then((data) =>{
+    console.log(data);
+    for (let index = 0; index < data.length; index++) {
+       users(
+        index,
+        data[index].username,
+        data[index].email,
+        data[index].createdAt.split('T')[0],
+        userId = data[index]._id
+       )
+        
+    }
+})
+.catch((error) => console.log(error))
+
+
+
+//ADD employees functions
+let addEmployeeBtn = document.getElementById('addEmployeeBtn')
+
+ addEmployeeBtn.onclick = function (e){
+    e.preventDefault()
+    let name = document.getElementById('employeeName')
+    let number = document.getElementById('employeeNumber')
+    let email = document.getElementById('employeeEmail')
+    let password = document.getElementById('employeePassword')
+    let confirmPassword = document.getElementById('confirmPassword')
+    let isAdmin = document.getElementById('isAdminChecked').checked
+
+    if (name.value != '' && number.value !='' && email.value !='' ) {
+        if(password.value === confirmPassword.value){
+            const results =  fetch('https://kayhans-backend-app.herokuapp.com/vehicleRecords/auth/register',{
+                method: 'POST',
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify({
+                    username:name.value,
+                    email:email.value,
+                    number:number.value,
+                    password:password.value,
+                    isAdmin:isAdmin
+                })
+                
+            }).then((res)=>res.json())
+            alert('Sales Person Successfully added')
+            window.location.href = '../dashboard.html'
+        } else{
+            window.alert('Passwords do not match!')
+
+        }
+    } else{
+        alert('All fields are required')
+    }
+}
+
+
+
+
+
+
+/*========END OF EMPLOYEES FUNCTIONS=========*/
+
+let btn = document.querySelector('.addTrip')
+btn.onclick = (e) =>{
+    e.preventDefault()
+    let vehicleName = document.getElementById('selectV').value.trim()
+    let fuel = document.getElementById('fuel').value.trim()
+    let weight = document.getElementById('weight').value.trim()
+    let rate = document.getElementById('rate').value.trim()
+    let other = document.getElementById('other').value.trim()
+    let dailySales = document.getElementById('dailySales').value.trim()
+    let road = document.getElementById('road').value.trim()
+    let totalExpenses = fuel + weight + other + road + rate
+
+    let sales = totalExpenses - dailySales
+    let body = {
+        vehicleNumber: vehicleName,
+        expenses:{
+        ratePerTongue: rate.value,
+        other: other.value ,  
+        raodExpenses: road,
+        fuel: fuel.value,
+        axelWeight: weight.value,
+        },
+        dailySales:dailySales,
+        totalExpenses: totalExpenses,
+        sales: sales
+    }
+    fetch(`https://kayhans-backend-app.herokuapp.com/vehicleRecords/trips`, {
+        method: 'POST',
+        mode: 'cors',
+        headers:{
+            'Accept':'*',
+            'Content-Type': 'application/json',
+            'token': `Bearer ${user1.accessToken}`,
+        },
+        body: JSON.stringify(body)
+        })
+        .then((res)=>res.json())
+        .then((data)=>{
+        console.log(data);
+        window.alert('data saved successfully')
+        window.location.href="../dashboard.html"
+    
+    })
+    .catch((error)=>{
+        window.alert(error)
+    })
+    }
+
+
+//     vehicleNumber: {type: String, required: true},
+    
+//     category:{
+//         type: String,
+//         required: true
+//     },
+//     expenses:{
+//         ratePerTongue:{
+//            type: Number,
+//            default: 0
+//         },
+//         fuel:{
+//             type: Number,
+//             default: 0
+//         },
+//         axelWeight:{
+//             type: Number,
+//             default: 0
+//         },
+//         roadExpenses:{
+//             type: Number,
+//             default:0
+//         },
+//         other:{
+//             type:Number,
+//             default: 0
+//         }
+        
+//     },
+//     totalExpenses:{ 
+//         type: Number,
+//         default: 0
+//     },
+//     dailySales: {
+//         type: Number,
+//         required: true
+//     },
+// sales:{
+//     type: Number,
+   
+// }
---
+++ controllers/editController.js
@@ -0,0 +1,119 @@
+window.addEventListener('load', function(){
+
+    const urlParams = new URLSearchParams(window.location.search)
+    const id = urlParams.get('id');
+    var _id;
+
+    let usernameValue =document.getElementById('employeeName')
+    let emailValue =document.querySelector('#employeeEmail')
+    let isAdmin = document.getElementById('adminPrivilages')
+    let editUsername = document.getElementById('fullName')
+    let editPhone = document.getElementById('Phone')
+    let editEmail = document.getElementById('Email')
+    let adminAlert = document.getElementById('adminAlert')
+    let adminSuccesAlert = document.getElementById('adminSuccesAlert')
+    let alertMessage = document.getElementById('alertMessage')
+
+
+    adminAlert.style.display="none"
+    adminSuccesAlert.style.display = "none"
+    let isAdminn;
+
+
+fetch(`https://kayhans-backend-app.herokuapp.com/vehicleRecords/users/${id}`)
+.then(response => response.json())
+.then(data =>{
+    console.log(data.username);
+    _id = data._id;
+    usernameValue.innerHTML = data.username
+    emailValue.innerHTML =data.email
+    emailValue.value = data.email
+    isAdmin.checked = data.isAdmin
+    editUsername.value = data.username
+    editEmail.value = data.email
+    editPhone.value = data.number
+    isAdminn = data.isAdmin
+    
+    
+})
+.catch((error)=>window.alert(error))
+
+const editButton = document.querySelector('#editDetailsBtn')
+editButton.onclick = function submit(e){
+    e.preventDefault()
+    // const username = document.getElementById('employeeName').value
+    // const email = document.getElementById('Email').value
+    // const phone = document.getElementById('Phone').value
+    
+    
+    // var isChecked=document.getElementById("isChecked").checked
+
+    if(editUsername.value != '' && editEmail.value != "" && editPhone.value != ""){
+         fetch(`https://kayhans-backend-app.herokuapp.com/vehicleRecords/users/${_id}`,{
+        method: 'PATCH',
+        Headers: {
+           
+            'Content-Type': 'application/json',    
+        },
+        mode: 'cors',
+        body: JSON.stringify({
+            username:editUsername.value,
+            email: editEmail.value,
+            number: editPhone.value
+            
+        })
+    })
+        .then((response)=>response.json())
+        .then((data)=>{
+            console.log(data);
+            window.alert('Profile Updated')
+            window.location.href = '../users.html'
+
+        })
+        .catch((error)=>{
+            window.alert(error)
+            window.location.reload()
+        })
+    } else{
+        window.alert("Passwords do not match")
+    }
+    }
+ 
+const editAdminBtn = this.document.getElementById('editAdminBtn')
+
+editAdminBtn.onclick = function (e){
+e.preventDefault()
+ if (isAdmin.checked == isAdminn) {
+    adminAlert.style.display = "flex"
+    alertMessage.innerHTML =`No changes made for user`
+    window.reload
+ }
+ else{
+    try {
+        fetch(`https://kayhans-backend-app.herokuapp.com/vehicleRecords/users/${_id}`,{
+        method: 'PATCH',
+        Headers: {
+           
+            'Content-Type': 'application/json',    
+        },
+        
+        body: JSON.stringify({
+            isAdmin:isAdminn.checked
+            
+        })
+    })
+    .then((response)=>response.json)
+    .then((data)=>{
+        adminSuccesAlert.style.display ="flex"
+        alertMessage.innerHTML = `Previlages updated for ${data.username}` 
+        window.reload
+    })
+    } catch (error) {
+        
+    }
+}
+}
+    
+
+
+})
